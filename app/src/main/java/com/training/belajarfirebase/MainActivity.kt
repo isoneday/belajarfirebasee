89025977bc7ee286220859ea68fbab54cee922dd
udakita <udakita11@gmail.com>
2018-10-31 21:43:28 +0700

    sadas

--- app/src/main/java/com/training/belajarfirebase/MainActivity.kt
+++ app/src/main/java/com/training/belajarfirebase/MainActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+//sip aja
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

47629d6e1820cd4b0947313b386fb0a42f8aee49
udakita <34660272+udakita@users.noreply.github.com>
2018-10-31 21:31:50 +0700

    update ya
    hanya testing saja

--- app/src/main/java/com/training/belajarfirebase/LoginActivity.kt
+++ app/src/main/java/com/training/belajarfirebase/LoginActivity.kt
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
-
+//this is just try
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
